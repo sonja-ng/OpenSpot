69012880611eb1ce62c589794295c11ccf5b6794
Sonja <sonja.ng1@gmail.com>
2021-12-30 20:40:36 -0500

    Connect Root component to entry file

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h1>Bench Bnb</h1>
+    </div>
+);
+
+export default App;
--- frontend/openspot.jsx
+++ frontend/openspot.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const store = configureStore();
@@ -8,6 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>hello from react</h1>, root)
+    ReactDOM.render(<Root store={ store }/>, root)
 });
 
