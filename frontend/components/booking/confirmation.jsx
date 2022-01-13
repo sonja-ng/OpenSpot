95e85eeba755601c03fb571f9c61892ab1e714cf
Sonja <sonja.ng1@gmail.com>
2022-01-12 23:23:53 -0500

    Style confirmation page

--- frontend/components/booking/confirmation.jsx
+++ frontend/components/booking/confirmation.jsx
@@ -7,11 +7,35 @@ class Confirmation extends React.Component {
 
     render(){
         return(
-            <h1>hello</h1>
+            <div className="confirmation-container">
+                <div className="confirm-bg">
+                    <div className="confirm-main">
+                        <div className="confirm-rest-name">CheLi</div>
+                        <div className="confirm-details">
+                            <div className="check">
+                               <img src={window.greencheckURL} alt="green check"/>
+                               <div>Reservation Confirmed</div>
+                            </div>
+                            <div className="flex-details">
+                                <div className="confirm-items"><i className="far fa-user"></i>2</div>
+                                <div className="confirm-items">
+                                <i className="far fa-calendar"></i>2022-02-10 at 1:30PM
+                                </div>  
+                            </div>
+                            <div className="confirm-button-row">
+                                <button className="c-button">Modify</button>
+                               <button className="c-button">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                    <aside className="user-info">
+                        <div className="user-fname"><i className="far fa-user"></i>Sonja Ng</div>
+                    </aside>
+                </div>
+                <div className="grey-bg"></div>
+            </div>
         )
     }
-
-
 }
 
 export default Confirmation;
