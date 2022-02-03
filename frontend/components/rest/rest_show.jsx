b1601983d8c252e389c1aedcd3c415c775d7b134
Sonja <sonja.ng1@gmail.com>
2022-02-03 17:48:32 -0500

    Fix rest show page overflow

--- frontend/components/rest/rest_show.jsx
+++ frontend/components/rest/rest_show.jsx
@@ -73,68 +73,70 @@ class RestShow extends React.Component {
                 <div className="rest_body">
                 <div className="main_rest_content">
                     <div id="overview"></div>
-                        <div className="rest-navbar">
-                            <HashLink to={`/rests/${rest.id}#overview`} className="content-button">Overview</HashLink>
-                            <HashLink to={`/rests/${rest.id}#photos`}  className="content-button">Photos</HashLink>
-                            <HashLink to={`/rests/${rest.id}#menu`} className="content-button">Menu</HashLink>
-                            <HashLink to={`/rests/${rest.id}#reviews`} className="content-button">Reviews</HashLink>
-                        </div>                    
-                        <div className="rest-title">
-                            {rest.name}
-                        </div>
-                        <div className="rest-info">
-                            <div>{ this.renderStars() }</div>
-                            <div>&#160;{ this.state.starRating.length }</div>
-                            <div className="rest-icons"><i className="far fa-comment-alt"></i><div className="text">{ this.props.reviews.length } reviews</div></div>
-                            <div className="rest-icons"><i className="far fa-money-bill-alt"></i><div className="text">30 and under</div></div>
-                            <div className="rest-icons"><i className="fas fa-utensils"></i><div className="text">{rest.cuisine}</div></div>
-                        </div>
-                        <div className="description">
-                            {rest.description}
-                        </div>
-                        <div id="photos"></div>
-                        <div className="subheader">
-                            9 Photos
-                        </div>
-                        <Gallery images={rest.photos} />
-                        <div id="menu"></div>
-                        <div className="subheader">
-                            Menu
-                        </div>
-                        <div className="item-cat">Popular Dish</div>
-                        <div className="menu-items">
-                            <div className="item-name">{rest.menu.popular}</div>
-                            <div className="item-price">{rest.menu.pop_price}</div>
-                        </div>
-                        <div className="item-cat">Appetizer</div>
-                        <div className="menu-items">
-                            <div className="item-name">{rest.menu.app}</div>
-                            <div className="item-price">{rest.menu.app_price}</div>
-                        </div>
-                        <div className="item-cat">Main</div>
-                        <div className="menu-items">
-                            <div className="item-name">{rest.menu.main}</div>
-                            <div className="item-price">{rest.menu.m_price}</div>
-                        </div>
-                        <div id="reviews"></div>
-                        <div className="subheader">
-                            What { this.props.reviews.length } people are saying
-                        </div>
-                        <div className="write-review"><button className="write-review-button" onClick={formAction}>Write a review</button></div>
-                        <ReviewFormContainer reviewOut={this.state.reviewOut} closeReview={this.closeReview}/>
-                        <ReviewIndexContainer />
+                    <div className="rest-navbar">
+                        <HashLink to={`/rests/${rest.id}#overview`} className="content-button">Overview</HashLink>
+                        <HashLink to={`/rests/${rest.id}#photos`}  className="content-button">Photos</HashLink>
+                        <HashLink to={`/rests/${rest.id}#menu`} className="content-button">Menu</HashLink>
+                        <HashLink to={`/rests/${rest.id}#reviews`} className="content-button">Reviews</HashLink>
+                    </div>                    
+                    <div className="rest-title">
+                        {rest.name}
                     </div>
-                    <aside className="right-content">
-                        <div className="rsvp-container">
-                            <RestBookingContainer />
-                        </div>
-                        <div>
-                        <Map rest={rest} />
-                        <div className="address">{rest.address},&#160;{rest.city},&#160;{rest.state}&#160;{rest.zip}</div>
-                        </div>
-                        <div className="misc">
-                        </div>
-                    </aside>
+                    <div className="rest-info">
+                        <div>{ this.renderStars() }</div>
+                        <div>&#160;{ this.state.starRating.length }</div>
+                        <div className="rest-icons"><i className="far fa-comment-alt"></i><div className="text">{ this.props.reviews.length } reviews</div></div>
+                        <div className="rest-icons"><i className="far fa-money-bill-alt"></i><div className="text">30 and under</div></div>
+                        <div className="rest-icons"><i className="fas fa-utensils"></i><div className="text">{rest.cuisine}</div></div>
+                    </div>
+                    <div className="description">
+                        {rest.description}
+                    </div>
+                    <div id="photos"></div>
+                    <div className="subheader">
+                        9 Photos
+                    </div>
+                    <Gallery images={rest.photos} />
+                    <div id="menu"></div>
+                    <div className="subheader">
+                        Menu
+                    </div>
+                    <div className="item-cat">Popular Dish</div>
+                    <div className="menu-items">
+                        <div className="item-name">{rest.menu.popular}</div>
+                        <div className="item-price">{rest.menu.pop_price}</div>
+                    </div>
+                    <div className="item-cat">Appetizer</div>
+                    <div className="menu-items">
+                        <div className="item-name">{rest.menu.app}</div>
+                        <div className="item-price">{rest.menu.app_price}</div>
+                    </div>
+                    <div className="item-cat">Main</div>
+                    <div className="menu-items">
+                        <div className="item-name">{rest.menu.main}</div>
+                        <div className="item-price">{rest.menu.m_price}</div>
+                    </div>
+                    <div id="reviews"></div>
+                    <div className="subheader">
+                        What { this.props.reviews.length } people are saying
+                    </div>
+                    <div className="write-review"><button className="write-review-button" onClick={formAction}>Write a review</button></div>
+                    <ReviewFormContainer reviewOut={this.state.reviewOut} closeReview={this.closeReview}/>
+                </div>
+                <div className="review-container">
+                    <ReviewIndexContainer />
+                </div>
+                <aside className="right-content">
+                    <div className="rsvp-container">
+                        <RestBookingContainer />
+                    </div>
+                    <div>
+                    <Map rest={rest} />
+                    <div className="address">{rest.address},&#160;{rest.city},&#160;{rest.state}&#160;{rest.zip}</div>
+                    </div>
+                    <div className="misc">
+                    </div>
+                </aside>
                 </div>
             </div>
         )
