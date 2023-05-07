8e3064fecb05fef749968d1c9559479460420d38
Sonja <sonja.ng1@gmail.com>
2023-05-07 16:44:50 -0400

    Edit render-build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,12 +3,6 @@
 set -o errexit
 
 bundle install
-<<<<<<< Updated upstream
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-=======
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
->>>>>>> Stashed changes
